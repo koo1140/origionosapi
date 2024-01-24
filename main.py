803dff067692dc9ca994dba3117db45dc251a857
Mmm <112756430+koo1140@users.noreply.github.com>
2024-01-24 17:04:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/qa')
 def question():
     question_param = request.args.get('question') 
     return jsonify({"question": question_param, "answer": "one lnol"})
 
-if name == 'main':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
