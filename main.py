50d25fffd62d71d851921cbc9c1c2ee528631e0a
Mmm <112756430+koo1140@users.noreply.github.com>
2024-01-24 16:53:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/qa')
 def question():
     question_param = request.args.get('question') 
-    return f'The value of the "question" parameter is: {question_param}
+    return f'The value of the "question" parameter is: {question_param}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
